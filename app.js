209e705b5fa7930ff34368bc55f41ce00bd5a2a7
Sakina Shivji <shivjisakina@gmail.com>
2017-06-03 21:35:30 -0400

    added tv show variable

--- app.js
+++ app.js
@@ -4,9 +4,9 @@
 // Push/render(?) their buttons onto the site
 // Pull the gifs from the api that have to do with that button
 // Show them on the website when the button is clicked
-// Enable ratings to show underneath each gif 
+// Enable ratings to show underneath each gif
 
 
 // TV Shows Variable
 
-var
+var tvshows = ["Greys Anatomy", "Friends", "How I Met Your Mother", "The Middle", "The Carrie Diaries", "The Fosters", "Chasing Life", "Two and a Half Men", "Gossip Girl", "The Originals", "2 Broke Girls", "Mike & Molly", "The Big Bang Theory"]
