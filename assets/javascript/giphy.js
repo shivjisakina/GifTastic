0a9db1371d2d2eb7000db45a58a960107dd0c71a
Sakina Shivji <shivjisakina@gmail.com>
2017-06-04 22:47:20 -0400

    removed old js file

--- assets/javascript/giphy.js
+++ assets/javascript/giphy.js
@@ -1,3 +1,12 @@
+// Pseudo Code:
+// Create initial buttons
+// Create input form so user can add their own tv shows
+// Push/render(?) their buttons onto the site
+// Pull the gifs from the api that have to do with that button
+// Show them on the website when the button is clicked
+// Enable ratings to show underneath each gif
+// Enable user to stop and start gif animation on click
+
 // Document ready function
 $(document).ready (function() {
     console.log("ready");
